7db33d9b8c8ccf81867615623cb5c154edfdfd34
Henry Wilder <henrythepony@gmail.com>
2023-05-13 18:56:45 -0400

    Added more compilation

---
+++ src/scripts/snippets/number-literal.ts
@@ -0,0 +1,12 @@
+export const ex = (value: number): string => `${value}`.replace('.',',');
+
+const rx = /([0-9,]+)/g;
+
+export const highlight = (text: string) => {
+    text = text.replace(rx, '<span class="literal">$1</span>');
+    return text;
+};
+
+export const compile = (text: string) => {
+    return text.replace(rx, '$1');
+};
