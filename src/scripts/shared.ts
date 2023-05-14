90d4757e7ca59db719894ce84bfc8c2044f75fcb
Henry Wilder <henrythepony@gmail.com>
2023-05-13 20:02:48 -0400

    Started working on useful prototype function

--- src/scripts/shared.ts
+++ src/scripts/shared.ts
@@ -1,10 +1,13 @@
 export { };
     
+type replacementType = string | ((group: string) => string);
+    
 declare global {
     interface String {
         before: (substring: string) => string;
         after: (substring: string) => string;
         between: (start: string, end: string) => string;
+        replaceWithin: (rx: RegExp, ...replacements: replacementType[]) => string;
     }
 }
 
@@ -29,3 +32,25 @@ declare global {
     return _this.after(start).before(end);
 };
 
+/**
+ * Regex replaces for each group in the regex.
+ */
+(String.prototype as any).replaceWithin = function (rx: RegExp, ...replacements: replacementType[]): string {
+    const _this: string = this as string;
+    let result: string = _this;
+    const segments: RegExpMatchArray[] = Array.from(result.matchAll(rx));
+    console.log(segments);
+    let lastEnd: number = 0;
+    let sectioned: string[] = [];
+    for (const segment of segments) {
+        const start: number = segment.index!;
+        const end: number = start + segment[0].length;
+        sectioned.push(_this.substring(lastEnd, start));
+        sectioned.push(segment[0]);
+        lastEnd = end;
+    }
+    sectioned.push(_this.substring(lastEnd));
+    console.log(sectioned);
+    result = sectioned.join('');
+    return result;
+};
